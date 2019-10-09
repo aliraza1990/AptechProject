7aa6e86335b92122163c3a5b2c1f5d61d66b15f9
asp <asp@Aptechnk.edu>
2019-10-09 12:59:38 +0500

    Rafia Work

--- WebApplication4/PartialClasses.cs
+++ WebApplication4/PartialClasses.cs
@@ -18,4 +18,28 @@ namespace WebApplication4
     public partial class timing
     {
     }
+    [MetadataType(typeof(BatchMetaData))]
+    public partial class batch
+    {
+    }
+    [MetadataType(typeof(CampusMetaData))]
+    public partial class campus
+    {
+    }
+    [MetadataType(typeof(CourseOfferedMeteData))]
+    public partial class coursesOffered
+    {
+    }
+    [MetadataType(typeof(curriculumMetaData))]
+    public partial class curriculum
+    {
+    }
+    [MetadataType(typeof(UsermetaData))]
+    public partial class user
+    {
+    }
+    [MetadataType(typeof(WalkinMetaData))]
+    public partial class walkin
+    {
+    }
 }
